2318c6785526a67bf61fd269b4f9863d99c6335f
Vo Duc Loi <shjnvo89@gmail.com>
2024-06-13 09:57:49 +0700

    remove comment code

--- app/controllers/v1/clients_controller.rb
+++ app/controllers/v1/clients_controller.rb
@@ -2,7 +2,6 @@ class V1::ClientsController < ApplicationController
   def create
     client = current_user.clients.build(client_params)
     if client.save
-      # assign_client_products(client) if params[:product_ids]
       render json:
         client.to_json(
           only: %i[name email payout_rate serect_key]
@@ -17,11 +16,4 @@ class V1::ClientsController < ApplicationController
   def client_params
     params.permit(:email, :name, :payout_rate)
   end
-
-  # def assign_client_products(client)
-  #   products = Product.includes(:brand).where(id: params[:product_ids] - client.products.pluck(:id))
-  #   products.each do |product|
-  #     Client::Product.create!(client:, product:, user: current_user)
-  #   end
-  # end
 end
